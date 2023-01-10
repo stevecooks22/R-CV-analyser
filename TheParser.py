ccc75437b2c1fd4ecb578be34980f3c97da56226
stevecooks22 <121758482+stevecooks22@users.noreply.github.com>
2023-01-10 11:39:28 -0500

    Add files via upload

---
+++ TheParser.py
@@ -0,0 +1,239 @@
+###### Packages Used ######
+import streamlit as st # core package used in this project
+import pandas as pd
+import base64, random
+import time,datetime
+import pymysql
+import os
+import socket
+import platform
+#import geocoder
+import secrets
+import io,random
+import plotly.express as px # to create visualisations at the admin session
+import plotly.graph_objects as go
+from geopy.geocoders import Nominatim
+# libraries used to parse the pdf files
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+# from streamlit_tags import st_tags
+from PIL import Image
+# pre stored data for prediction purposes
+# from Courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
+# import nltk
+# nltk.download('stopwords')
+
+
+###### Preprocessing functions ######
+
+
+# Generates a link allowing the data in a given panda dataframe to be downloaded in csv format 
+def get_csv_download_link(df,filename,text):
+    csv = df.to_csv(index=False)
+    ## bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()      
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
+
+
+# Reads Pdf file and check_extractable
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    ## close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+# show uploaded file path to view pdf_display
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+# course recommendations which has data already loaded from Courses.py
+def course_recommender(course_list):
+    st.subheader("**Courses & Certificates Recommendations 👨‍🎓**")
+    c = 0
+    rec_course = []
+    ## slider to choose from range 1-10
+    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 5)
+    random.shuffle(course_list)
+    for c_name, c_link in course_list:
+        c += 1
+        st.markdown(f"({c}) [{c_name}]({c_link})")
+        rec_course.append(c_name)
+        if c == no_of_reco:
+            break
+    return rec_course
+
+
+###### Database Stuffs ######
+
+
+# sql connector
+connection = pymysql.connect(host='192.168.50.81',user='root',password='',db='ob',port=3306)
+cursor = connection.cursor()
+
+
+# inserting miscellaneous data, fetched results, prediction and recommendation into user_data table
+def insert_data(sec_token,ip_add,host_name,dev_user,os_name_ver,latlong,city,state,country,act_name,act_mail,act_mob,name,email,res_score,timestamp,no_of_pages,reco_field,cand_level,skills,recommended_skills,courses,pdf_name):
+    DB_table_name = 'user_data'
+    insert_sql = "insert into " + DB_table_name + """
+    values (0,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    rec_values = (str(sec_token),str(ip_add),host_name,dev_user,os_name_ver,str(latlong),city,state,country,act_name,act_mail,act_mob,name,email,str(res_score),timestamp,str(no_of_pages),reco_field,cand_level,skills,recommended_skills,courses,pdf_name)
+    cursor.execute(insert_sql, rec_values)
+    connection.commit()
+
+
+# inserting feedback data into user_feedback table
+def insertf_data(feed_name,feed_email,feed_score,comments,Timestamp):
+    DBf_table_name = 'user_feedback'
+    insertfeed_sql = "insert into " + DBf_table_name + """
+    values (0,%s,%s,%s,%s,%s)"""
+    rec_values = (feed_name, feed_email, feed_score, comments, Timestamp)
+    cursor.execute(insertfeed_sql, rec_values)
+    connection.commit()
+
+
+###### Setting Page Configuration (favicon, Logo, Title) ######
+
+
+st.set_page_config(
+   page_title="AI Resume Analyzer",
+   page_icon='./Logo/recommend.png',
+)
+
+
+###### Main function run() ######
+
+
+def run():
+    
+    # (Logo, Heading, Sidebar etc)
+    img = Image.open('./Logo/RESUM.png')
+    st.image(img)
+    st.sidebar.markdown("# Choose Something...")
+    activities = ["User", "Feedback", "About", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    # link = '<b>Built with 🤍 by <a href="https://dnoobnerd.netlify.app/" style="text-decoration: none; color: #021659;">Deepak Padhi</a></b>' 
+    # st.sidebar.markdown(link, unsafe_allow_html=True)
+    # st.sidebar.markdown('''
+    #     <!-- site visitors -->
+
+    #     <div id="sfct2xghr8ak6lfqt3kgru233378jya38dy" hidden></div>
+
+    #     <noscript>
+    #         <a href="https://www.freecounterstat.com" title="hit counter">
+    #             <img src="https://counter9.stat.ovh/private/freecounterstat.php?c=t2xghr8ak6lfqt3kgru233378jya38dy" border="0" title="hit counter" alt="hit counter"> -->
+    #         </a>
+    #     </noscript>
+    
+    #     <p>Visitors <img src="https://counter9.stat.ovh/private/freecounterstat.php?c=t2xghr8ak6lfqt3kgru233378jya38dy" title="Free Counter" Alt="web counter" width="60px"  border="0" /></p>
+    
+    # ''', unsafe_allow_html=True)
+
+    ###### Creating Database and Table ######
+
+
+    # Create the DB
+    db_sql = """CREATE DATABASE IF NOT EXISTS OB;"""
+    cursor.execute(db_sql)
+
+
+    # Create table user_data and user_feedback
+    DB_table_name = 'user_data'
+    table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                    sec_token varchar(20) NOT NULL,
+                    ip_add varchar(50) NULL,
+                    host_name varchar(50) NULL,
+                    dev_user varchar(50) NULL,
+                    os_name_ver varchar(50) NULL,
+                    latlong varchar(50) NULL,
+                    city varchar(50) NULL,
+                    state varchar(50) NULL,
+                    country varchar(50) NULL,
+                    act_name varchar(50) NOT NULL,
+                    act_mail varchar(50) NOT NULL,
+                    act_mob varchar(20) NOT NULL,
+                    Name varchar(500) NOT NULL,
+                    Email_ID VARCHAR(500) NOT NULL,
+                    resume_score VARCHAR(8) NOT NULL,
+                    Timestamp VARCHAR(50) NOT NULL,
+                    Page_no VARCHAR(5) NOT NULL,
+                    Predicted_Field BLOB NOT NULL,
+                    User_level BLOB NOT NULL,
+                    Actual_skills BLOB NOT NULL,
+                    Recommended_skills BLOB NOT NULL,
+                    Recommended_courses BLOB NOT NULL,
+                    pdf_name varchar(50) NOT NULL,
+                    PRIMARY KEY (ID)
+                    );
+                """
+    cursor.execute(table_sql)
+
+
+    DBf_table_name = 'user_feedback'
+    tablef_sql = "CREATE TABLE IF NOT EXISTS " + DBf_table_name + """
+                    (ID INT NOT NULL AUTO_INCREMENT,
+                        feed_name varchar(50) NOT NULL,
+                        feed_email VARCHAR(50) NOT NULL,
+                        feed_score VARCHAR(5) NOT NULL,
+                        comments VARCHAR(100) NULL,
+                        Timestamp VARCHAR(50) NOT NULL,
+                        PRIMARY KEY (ID)
+                    );
+                """
+    cursor.execute(tablef_sql)
+
+    # ### saving the uploaded resume to folder
+    # save_image_path = './Uploaded_Resumes/'+pdf_file.name
+    # pdf_name = pdf_file.name
+    # with open(save_image_path, "wb") as f:
+    #             f.write(pdf_file.getbuffer())
+    #             show_pdf(save_image_path)                    
+    
+save_image_path = "/Users/stephencooks/pyapps/AI-Resume-Analyser-main/App/Uploaded_Resumes"
+
+
+### parsing and extracting whole resume 
+resume_data = ResumeParser(save_image_path).get_extracted_data()
+if resume_data:
+                
+                ## Get the whole resume data into resume_text
+                resume_text = pdf_reader(save_image_path)
+
+                ## Showing Analyzed data from (resume_data)
+                st.header("**Resume Analysis 🤘**")
+                st.success("Hello "+ resume_data['name'])
+                st.subheader("**Your Basic info 👀**")
+                try:
+                    st.text('Name: '+resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Degree: '+str(resume_data['degree']))                    
+                    st.text('Resume pages: '+str(resume_data['no_of_pages']))
+
+                except:
+                    pass             
+
+# Calling the main (run()) function to make the whole process run
+run()
